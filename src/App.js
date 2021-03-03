57ba4e783672691db328262db69d3931f4c642a3
Segun <shegunolanitori@gmail.com>
2021-03-03 20:31:11 +0100

    clean ups

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React from "react";
 import "./App.css";
 import BubbleChart from "./components/Bubble";
-// import ScatterChart from "./components/Scatter";
-// import { ChakraProvider } from "@chakra-ui/react"
 
 function App() {
   return (
     <div>
       <BubbleChart />
-      {/* <ScatterChart /> */}
     </div>
   );
 }
